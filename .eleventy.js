ed137b1d17bcb445c38a06d9fb175de296c309e6
Tushar <tushar.gugnani@gmail.com>
2022-07-29 12:26:02 +0530

    sidebar

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,11 @@
 module.exports = function(eleventyConfig) {
+
+    const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+
+
+    //Add plugin
+    eleventyConfig.addPlugin(eleventyNavigationPlugin);
+
     // Input Directory : src Output directory: _site
 
     eleventyConfig.addPassthroughCopy("src/css");
